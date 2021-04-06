6fa0d5966299c0bd76c232c80b9d612e798ea68a
Mike Thurgood <mike.thurgood@wintechnologies.net>
2021-04-06 23:10:18 +0100

    configure hot reloads for styled components

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Navigation from '../src/components/Navigation'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    );
+
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
 
   render() {
     return (
-      <Html lang="en-GB" >
-        <Head />
+      <Html>
+        <Head>
+          {this.props.styleTags}
+        </Head>
         <body>
-            <Main />
-            <NextScript />
+          <Main />
+          <NextScript />
         </body>
       </Html>
-    )
+    );
   }
-}
-
-export default MyDocument
+};
